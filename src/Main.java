06cf02558255f7439c17bb98c5a55121b71ba8a9
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-19 19:14:08 +0500

    Dividers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Введи значение a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введи значение b: ");
+        int b = scanner.nextInt();
+
+        int maxNumber = a;
+        int maxDivisorSum = 0;
+        try {
+
+            if (a < b) {
+
+                for (int number = a; number <= b; number++) {
+                    int divisorSum = 0;
+                    for (int divisor = 1; divisor <= number / 2; divisor++) {
+                        if (number % divisor == 0) {
+                            divisorSum += divisor;
+                        }
+                    }
+
+                    if (divisorSum > maxDivisorSum) {
+                        maxDivisorSum = divisorSum;
+                        maxNumber = number;
+                    }
+                }
+
+                System.out.println("Число с максимальной суммой делителей: " + maxNumber);
+                System.out.println("Сумма делителей этого числа: " + maxDivisorSum);
+            } else {
+                System.out.println("Значение а должно быть меньше b");
+            }
+        }catch (Exception ex) {
+            System.out.println("Ты ввёл что-то не так");
+        }
     }
 }
